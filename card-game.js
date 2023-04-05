2d1fb232b8515bbd9161cdbfd3d7b3be6d64a4af
Haohaoscreamandrun <j842671395@gmail.com>
2023-04-05 14:50:46 +0800

    complete

--- card-game.js
+++ card-game.js
@@ -1,3 +1,13 @@
+
+const GAME_STATE = {
+  FirstCardAwait: "FirstCardAwait",
+  SecondCardsAwait: "SecondCardsAwait",
+  CardsMatchFailed: "CardsMatchFailed",
+  CardsMatchSuccess: "CardsMatchSuccess",
+  GameFinished: "GameFinished"
+}
+
+
 const Symbols = [
   'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // 黑桃
   'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
@@ -14,24 +24,24 @@ const view = {
     `
   },
 
-  getCardContent(index){
+  getCardContent(index) {
     const number = this.transformNumber((index + 1) % 13);
     const symbol = Symbols[Math.floor(index / 13)];
     return rawHTML =
-    `
+      `
     <p class="number">${number}</p>
     <img src=${symbol} alt="" class="suits">
     <p class="number">${number}</p>
     `
   },
 
-  displayCard() {
+  displayCard(indexes) {
     const cards = document.querySelector('.cards')
     // const array52 = Array.from(Array(52).keys())
     //Array(52)創造一個長度為52的空陣列，keys()將其轉變為陣列迭代器(或稱可迭代物件)
     //Array.from()以會從類陣列（array-like）或是可迭代（iterable）物件建立一個新的 Array 實體。
-    const array52 = utility.getRandomNumberArray(52)
-    const arrayHTML = array52.map(index => this.getCardElement(index))
+    // const array52 = utility.getRandomNumberArray(52)
+    const arrayHTML = indexes.map(index => this.getCardElement(index))
     cards.innerHTML = arrayHTML.join('')
     //join將陣列中所有元素迭代粘貼
   },
@@ -51,9 +61,10 @@ const view = {
     }
   },
 
-  flipCard(card) {
-    console.log(card)
-    if (card.classList.contains('back')) {
+  flipCards(...cards) {
+    //...在這裡叫其餘參數，其後的參數會被收集成為一個陣列,有length屬性並可以使用array function。用於不確定會傳入多少參數時。 
+    cards.map(card => {
+      if (card.classList.contains('back')) {
       // 回傳正面
       card.classList.remove('back')
       card.innerHTML = this.getCardContent(card.dataset.index)
@@ -62,7 +73,48 @@ const view = {
     // 回傳背面
     card.classList.add('back')
     card.innerHTML = null
-  }
+    })
+  },
+
+  pairedCards(...cards) {
+    cards.map(card=>{
+      card.classList.add('paired')
+    })
+  },
+
+  renderScore(score) {
+    document.querySelector('.score').innerHTML = `Score: ${score}`
+  },
+
+  renderTryTimes(times) {
+    document.querySelector('.tried').innerHTML = `You've tried ${times} times`
+  },
+
+  appendAnimation(...cards){
+    cards.map(card=>{
+      card.classList.add('wrong')
+      //remove class after 1s
+      card.addEventListener('animationend', event => {
+        card.classList.remove('wrong')
+      },{
+        once: true
+        //remove eventListener after use, or it will hang lots of.
+      })
+    })
+  },
+
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.tryTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
+  },
+
 }
 
 const utility = {
@@ -81,10 +133,81 @@ const utility = {
   }
 }
 
-view.displayCard()
+const controller = {
+  currentState: GAME_STATE.FirstCardAwait,
+
+  generateCards() {
+    view.displayCard(utility.getRandomNumberArray(52))
+  },
+
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) {
+      return
+    }
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwait:
+        view.flipCards(card)
+        //傳入單一值，被其餘參數轉變為長度為1的陣列
+        model.revealCards.push(card)
+        this.currentState = GAME_STATE.SecondCardsAwait;
+        break;
+      case GAME_STATE.SecondCardsAwait:
+        view.flipCards(card)
+        model.revealCards.push(card)
+        view.renderTryTimes(++model.tryTimes)
+        //前置 ++ 是先將變量的值加1，然後使用加1後的值參與運算；而後置 ++ 是先使用該值參與運算，然後再將值加1.
+        if (model.isRevealedCardsMatch()) {
+          //if match
+          view.renderScore(model.score += 10)
+          this.currentState = GAME_STATE.CardsMatchSuccess
+          view.pairedCards(...model.revealCards)
+          //將類陣列用展開運算子展開，進入函釋後又被其餘運算子轉成陣列
+          model.revealCards = [];
+          if (model.score === 260) {
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()  // 加在這裡
+            return
+          }
+          this.currentState = GAME_STATE.FirstCardAwait
+        } else {
+          //if not match
+          this.currentState = GAME_STATE.CardsMatchFailed
+          //run wrong animation
+          view.appendAnimation(...model.revealCards)
+          //留一點時間給使用者記憶
+          setTimeout(this.resetCards, 1000);
+          //1000ms equals 1s
+          //要計時的是函式本身不是呼叫的結果，不用加()
+        };
+        break;
+    }
+  },
+
+  resetCards(){
+    view.flipCards(...model.revealCards)
+    model.revealCards = [];
+    controller.currentState = GAME_STATE.FirstCardAwait;
+    //在這裡用this會call到setTimeout
+  },
+}
+
+const model = {
+  revealCards: [],
+
+  isRevealedCardsMatch() {
+    return (this.revealCards[0].dataset.index % 13 === this.revealCards[1].dataset.index % 13)
+  },
+
+  score: 0,
+  tryTimes: 0,
+}
+
+controller.generateCards()
 
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', event => {
-    view.flipCard(card)
+    controller.dispatchCardAction(card)
+    console.log(controller.currentState, model.revealCards)
   })
 })
